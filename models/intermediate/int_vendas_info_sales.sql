3dce5681388d10e31ea848cabf90441a7d672f3f
Gabriel_Ferraz <gabrielcma_@hotmail.com>
2025-02-24 11:47:29 -0300

    Ajustes feitos nas tabelas intermediarias

--- models/intermediate/int_vendas_info_sales.sql
+++ models/intermediate/int_vendas_info_sales.sql
@@ -34,29 +34,29 @@ with
     )
 ,   metricas as (
         select distinct
-        SK_SALES
-        , PK_SALES_ORDER
-        , PK_SALES_ORDER_DETAIL
-        , FK_CUSTOMER
-        , FK_TERRITORY
-        , FK_PRODUCT
-        , FK_CREDIT_CARD
-        , ORDER_DATE
-        , STATUS
-        , ONLINE_ORDER_FLAG
-        , ORDER_QTY
-        , UNIT_PRICE
-        , UNIT_PRICE_DISCOUNT        
-        , SUBTOTAL
-        , TAXA_MT
-        , FREIGHT
+        sk_sales
+        , pk_sales_order as fk_sales_order
+        , pk_sales_order_detail as fk_sales_order_detail
+        , fk_customer
+        , fk_territory
+        , fk_product
+        , fk_credit_card
+        , order_date
+        , status
+        , online_order_flag
+        , order_qty
+        , unit_price
+        , unit_price_discount        
+        , subtotal
+        , taxa_mt
+        , freight
         , total_purchase
-        , cast(UNIT_PRICE * ORDER_QTY as numeric(38,2)) as Valor_total_negociado
-        , cast(UNIT_PRICE * ORDER_QTY * (1 - UNIT_PRICE_DISCOUNT) as numeric(38,2)) as Valor_total_liquido
-        , cast(SUM(UNIT_PRICE * ORDER_QTY) 
-                OVER () / COUNT(pk_sales_order) OVER () as numeric(38,2)) AS ticket_medio_bruto
-        , cast(SUM(UNIT_PRICE * ORDER_QTY * (1 - UNIT_PRICE_DISCOUNT)) 
-                OVER () / COUNT(pk_sales_order) OVER () as numeric(38,2)) AS ticket_medio_liquido
+        , cast(unit_price * order_qty as numeric(38,2)) as valor_total_negociado
+        , cast(unit_price * order_qty * (1 - unit_price_discount) as numeric(38,2)) as valor_total_liquido
+        , cast(sum(unit_price * order_qty) 
+                over () / count(pk_sales_order) over () as numeric(38,2)) as ticket_medio_bruto
+        , cast(sum(unit_price * order_qty * (1 - unit_price_discount)) 
+                over () / count(pk_sales_order) over () as numeric(38,2)) as ticket_medio_liquido
         from join_sales
 
     )
